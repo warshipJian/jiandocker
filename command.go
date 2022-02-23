326a29b492a29add5b457f6fb983af08e35155db
warshipJian <J.GY.jian@gmail.com>
2022-02-23 08:58:53 +0000

    aufs day 2

--- command.go
+++ command.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"os/exec"
 	"syscall"
 
 	log "github.com/sirupsen/logrus"
@@ -21,54 +19,26 @@ var runCommand = cli.Command{
 		},
 	},
 	Action: func(context *cli.Context) error {
-		if len(context.Args()) < 1 {
-			return fmt.Errorf("Missing container command")
-		}
-
-		fmt.Println("aaa")
-
-		command := context.Args().Get(0)
-		args := []string{"init", command}
-		cmd := exec.Command("/proc/self/exe", args...)
-		cmd.SysProcAttr = &syscall.SysProcAttr{
-			Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS |
-				syscall.CLONE_NEWNET | syscall.CLONE_NEWIPC,
-		}
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		if err := cmd.Start(); err != nil {
-			log.Error(err)
-		}
-		limitMemory := context.String("m")
-		if limitMemory != "" {
-			MemoryLimit(cmd.Process.Pid, limitMemory)
-		}
-		cmd.Wait()
+		NewParentProcess(context)
 		return nil
 	},
 }
 
 var initCommand = cli.Command{
 	Name:  "init",
-	Usage: `mount proc system `,
+	Usage: `mount proc system busybox`,
 	Action: func(context *cli.Context) error {
-
-		fmt.Println("bbb")
-
-		command := context.Args().Get(0)
-		argv := []string{command}
-		fmt.Println(argv)
-
-		// unshareM()
 
 		// 挂载
 		setUpMount()
 
-		// 执行命令
+		// 执行传入的命令
+		command := context.Args().Get(0)
+		argv := []string{command}
 		if err := syscall.Exec(command, argv, os.Environ()); err != nil {
 			log.Errorf(err.Error())
 		}
+
 		return nil
 	},
 }
---
+++ run.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+)
+
+func NewParentProcess(context *cli.Context) {
+	command := context.Args().Get(0)
+	args := []string{"init", command}
+	cmd := exec.Command("/proc/self/exe", args...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET | syscall.CLONE_NEWIPC,
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Start(); err != nil {
+		log.Error(err)
+	}
+	limitMemory := context.String("m")
+	if limitMemory != "" {
+		MemoryLimit(cmd.Process.Pid, limitMemory)
+	}
+	cmd.Wait()
+}
